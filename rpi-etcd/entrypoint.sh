0925a0f5f1600ef41c51ac7411dfb08cd8677e36
Saphoooo <stephane.beuret@gmail.com>
2016-07-31 11:12:11 +0000

    modify entrypoint help

--- rpi-etcd/entrypoint.sh
+++ rpi-etcd/entrypoint.sh
@@ -3,8 +3,7 @@ HELP=" - POS: must be a number between 0 and 2, 0 for the first member, 1 for th
  - IP_MEMBER: is the IP address of the current node \n
  - IP_MEMBER_1: the IP address of the first member\n
  - IP_MEMBER_2: the IP address of the second member\n
- - IP_MEMBER_3: the IP address of the third member\n
-eg: sudo docker run -d --restart=always -p 4001:4001 -p 7001:7001 -p 2380:2380 -p 2379:2379 -e POS=0 -e IP_MEMBER=192.168.0.1 -e IP_MEMBER_1=192.168.0.1 -e IP_MEMBER_2=192.168.0.2 -e IP_MEMBER_2=192.168.0.3 -v /var/etcd:/var/etcd de13/rpi-etcd"
+ - IP_MEMBER_3: the IP address of the third member"
 
 if [ ! -z "${POS}" ] || (( "${POS}" > "2" )) ; then echo -e ${HELP} ; fi
 if [ ! -z "${IP_MEMBER}" ] ; then echo -e ${HELP} ; fi
